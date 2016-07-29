050bdcd5daf349bb87b6ddac9ff5402234081700
Mike Engel <mike@mike-engel.com>
2016-07-28 22:22:27 -0600

    Color refactor

--- index.js
+++ index.js
@@ -1,41 +1,68 @@
-const backgroundColor = '#2b303b'
-const foregroundColor = '#dfe1e8'
-const borderColor = backgroundColor
-const cursorColor = foregroundColor
-const colors = [
-  backgroundColor,
-  '#bf616a',
-  '#a3be8c',
-  '#ebcb8b',
-  '#8fa1b3',
-  '#b48ead',
-  '#96b5b4',
-  foregroundColor,
-  '#65737e',
-  '#d08770',
-  '#343d46',
-  '#4f5b66',
-  '#c0c5ce',
-  '#dfe1e8',
-  '#ab7967',
-  '#eff1f5'
-]
+const gray0 = '#2B303B'
+const gray1 = '#343D46'
+const gray2 = '#4F5B66'
+const gray3 = '#65737E'
+const gray4 = '#A7ADBA'
+const gray5 = '#C0C5CE'
+const gray6 = '#DFE1E8'
+const gray7 = '#EFF1F5'
+const red = '#BF616A'
+const green = '#A3BE8C'
+const yellow = '#EBCB8B'
+const blue = '#8FA1B3'
+const magenta = '#B48EAD'
+const cyan = '#96B5B4'
+const orange = '#D08770'
+const rust = '#AB7967'
+
+const colors = {
+  black: gray2,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white: gray6,
+  lightBlack: gray3,
+  lightRed: orange,
+  lightGreen: gray1,
+  lightYellow: gray2,
+  lightBlue: gray5,
+  lightMagenta: gray6,
+  lightCyan: rust,
+  lightWhite: gray7
+}
 
 exports.decorateConfig = (config) => {
+  const colorsArray = []
+
+  if (Array.isArray(config.colors)) {
+    for (let color in colors) {
+      if (colors.hasOwnProperty(color)) colorsArray.push(colors[color])
+    }
+  }
+
   return Object.assign({}, config, {
-    backgroundColor,
-    borderColor,
-    foregroundColor,
-    cursorColor,
-    colors,
+    backgroundColor: gray0,
+    borderColor: gray0,
+    foregroundColor: gray6,
+    cursorColor: gray6,
+    colors: colorsArray.length ? colorsArray : colors,
     css: `
       ${config.css || ''}
+
       .tab_tab {
         border-bottom: 4px solid transparent !important;
         transition: border-bottom-color 250ms !important;
       }
+
       .tab_tab.tab_active {
-        border-bottom-color: ${foregroundColor} !important;
+        border-bottom-color: ${gray6} !important;
+      }
+
+      .hasActivity {
+        color: ${magenta} !important;
       }
     `
   })
